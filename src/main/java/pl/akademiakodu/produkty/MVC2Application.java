45ede6cc8ab7e613ef79a0c126a8a9dafd142fdf
Monika Maria <monika.kocot9@gmail.com>
2019-03-14 12:27:14 +0100

    Link for show categories.

--- src/main/java/pl/akademiakodu/produkty/MVC2Application.java
+++ src/main/java/pl/akademiakodu/produkty/MVC2Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProduktyApplication {
+public class MVC2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProduktyApplication.class, args);
+		SpringApplication.run(MVC2Application.class, args);
 	}
 }
--- src/test/java/pl/akademiakodu/produkty/MVC2ApplicationTests.java
+++ src/test/java/pl/akademiakodu/produkty/MVC2ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProduktyApplicationTests {
+public class MVC2ApplicationTests {
 
 	@Test
 	public void contextLoads() {
